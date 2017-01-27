0085173c1a7e012d2bb2a41f25281002e14281fb
Chong Shen <shenchong1994@gmail.com>
2017-01-26 21:20:53 -0500

    setup byte 2

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+import json
+import urllib
+
+# this is used for constructing URLs to google's APIS
+from googleapiclient.discovery import build
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+#service = build('fusiontables', 'v1', developerKey=API_KEY)
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
