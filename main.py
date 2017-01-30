bfc3770e2f6500d92e2c0e2cbd3f86b2f1796bbe
Chong Shen <shenchong1994@gmail.com>
2017-01-30 13:19:04 -0500

    base code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Imports
+ # Imports
 import os
 import jinja2
 import webapp2
@@ -21,18 +21,60 @@ TABLE_ID = '1JobEuJSRuLuxxXgMXrVOdgPA7K9FMJVdOYZ3SgQ'
 # fusion tables API using the developer key
 service = build('fusiontables', 'v1', developerKey=API_KEY)
 
+
+
+# Import the Flask Framework
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+def get_all_data(query):
+	#request = service.table().get(tableId=TABLE_ID) 
+	response = service.query().sql(sql=query).execute()
+	return response
+
+# Default columns for the query
+query_cols = []
+query_animals = ['China']
+
+# make a query given a set of columns to retrieve
+def make_query(cols, countries, limit):
+	string_cols = ""
+	if cols == []:
+		cols = ['*']
+	for col in cols:
+		string_cols = string_cols + ", " + col
+	string_cols = string_cols[2:len(string_cols)] #why start with second?
+	
+	string_countries = ""
+	for country in countries:
+		string_countries = string_countries + country
+	string_countries = string_countries[2:len(string_countries)]
+	
+	query = "SELECT" + string_cols + " FROM " + TABLE_ID + " WHERE Country = '" + string_countries + "'"
+	query = query + " LIMIT " + str(limit)
+	
+	logging.info(query)
+	return query
 
 
 @app.route('/')
-def hello():
-    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-    return template.render()
+def get_all_data(self):
+    query = "SELECT * FROM " + TABLE_ID + " WHERE  Country = 'China' LIMIT 2"
+    response = service.query().sql(sql=query).execute()
+    logging.info(response['columns'])
+    logging.info(response['rows'])
+        
+    return response
+
+
+#@app.route('/')
+#def index():
+#    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+#    request = service.column().list(tableId = TABLE_ID)
+#    allheaders = get_all_data(make_query([], query_countries, 1))
+#    logging.info('allheaders') #what is logging.info?
+#    return template.render(allheaders = allheaders['columns'])
 
 
 @app.errorhandler(404)
